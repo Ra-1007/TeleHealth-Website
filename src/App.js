0e66b42d2b17402a66ea01e3638651ae136be470
shreya64684 <shreyashridhar107@gmail.com>
2024-02-09 11:43:32 +0530

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import ResponsiveDrawer from './components/dashboard/ResponsiveDrawer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Home from './components/login/home';
+import Login from './components/login/Login';
+import { Router } from '@mui/icons-material';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState("")
+
   return (
     <div className="App">
-      
+      <ResponsiveDrawer/>
+      <Home></Home>
+      <Router>
+        <Routes>
+      <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+          </Routes>
+
+      </Router>
     </div>
   );
 }
